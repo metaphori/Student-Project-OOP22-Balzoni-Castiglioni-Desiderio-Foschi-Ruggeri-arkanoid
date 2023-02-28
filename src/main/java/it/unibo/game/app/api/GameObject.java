8882df1f04674bf7bce359421ed834ca809b9926
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-02-28 18:14:56 +0100

    interface descrive a simple GameObject

---
+++ src/main/java/it/unibo/game/app/api/GameObject.java
@@ -0,0 +1,15 @@
+package it.unibo.game.app.api;
+
+import it.unibo.game.Pair;
+
+public interface GameObject {
+    /*
+     * game object deve implementare tutti i metodi degli oggetti
+     * di gioco considerando che siano statici
+     */
+    void setPos(Pair<Double, Double> pos);
+
+    Pair<Double, Double> getPos();
+
+    BoundingBox getBox();
+}
