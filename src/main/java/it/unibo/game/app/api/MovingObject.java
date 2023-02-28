57a489f6063298546c500646f06c8e8a413a51d2
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-02-28 18:15:57 +0100

    drescriving moving Object

---
+++ src/main/java/it/unibo/game/app/api/MovingObject.java
@@ -0,0 +1,7 @@
+package it.unibo.game.app.api;
+/*la calsse Speeed sono cazzi della Virginia */
+public interface MovingObject extends GameObject {
+    
+    //void SetSpeed(Speed s);
+    //Speed getSpeed();
+}
