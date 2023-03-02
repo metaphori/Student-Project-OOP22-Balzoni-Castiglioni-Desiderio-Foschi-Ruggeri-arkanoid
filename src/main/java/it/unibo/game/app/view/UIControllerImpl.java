63a6cc8314a4be4e2dd97506490741066e51c9b9
unknown <margherita.balzoni@studio.unibo.it>
2023-03-02 18:48:11 +0100

    UI controller implementation

---
+++ src/main/java/it/unibo/game/app/view/UIControllerImpl.java
@@ -0,0 +1,12 @@
+package it.unibo.game.app.view;
+
+import it.unibo.game.app.api.UIController;
+public class UIControllerImpl implements UIController{
+
+    @Override
+    public void chargeView(PAGES p) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'chargeView'");
+    }
+    
+}
