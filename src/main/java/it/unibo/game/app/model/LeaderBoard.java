fdbdb99880546a2828d0ba464c09a78105a8a59b
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-02 11:30:08 +0100

    leaderboard storage data of players

---
+++ src/main/java/it/unibo/game/app/model/LeaderBoard.java
@@ -0,0 +1,15 @@
+package it.unibo.game.app.model;
+import it.unibo.game.Pair;
+import java.util.*;
+
+public interface LeaderBoard {
+
+    boolean isPresent(String usr);
+
+    void updatePoints(String name, Integer points, Integer levelId, Integer roundId);
+
+    Integer getPosition(String name);
+
+    List<Pair<String,Integer>> getBestFive();
+
+}
