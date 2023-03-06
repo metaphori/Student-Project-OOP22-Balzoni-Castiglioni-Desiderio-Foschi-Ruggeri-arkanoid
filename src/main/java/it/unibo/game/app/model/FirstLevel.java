52428234baae7bcebe46134a2176ff723ff2b17b
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-06 16:26:42 +0100

    Created RoundEasy class and FirstLevel class

---
+++ src/main/java/it/unibo/game/app/model/FirstLevel.java
@@ -0,0 +1,23 @@
+package it.unibo.game.app.model;
+
+public class FirstLevel extends Level{
+
+    @Override
+    protected void setFirstRound() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setFirstRound'");
+    }
+
+    @Override
+    protected void setSecondRound() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSecondRound'");
+    }
+
+    @Override
+    protected void setThirdRound() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setThirdRound'");
+    }
+    
+}
---
+++ src/main/java/it/unibo/game/app/model/RoundEasy.java
@@ -0,0 +1,16 @@
+package it.unibo.game.app.model;
+
+public class RoundEasy extends AbstractRound {
+
+    public RoundEasy(int jump, int numB, int numS, int bH, int bW) {
+        super(jump, numB, numS, bH, bW);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void setPosBrick() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPosBrick'");
+    }
+    
+}
