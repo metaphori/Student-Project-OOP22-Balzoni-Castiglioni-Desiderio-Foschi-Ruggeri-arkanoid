4673c79ef3280b633ddcdfafcd797eded9463a4f
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-08 20:04:04 +0100

    Implemented FirstLevel

--- src/main/java/it/unibo/game/app/model/FirstLevel.java
+++ src/main/java/it/unibo/game/app/model/FirstLevel.java
@@ -2,22 +2,37 @@ package it.unibo.game.app.model;
 
 public class FirstLevel extends Level{
 
+    private final static int NORMAL_FIRST = 15;
+    private final static int NORMAL_SECOND = 20;
+    private final static int NORMAL_THIRD = 25;
+    private final static int SURPRISE_FIRST = 2; 
+    private final static int SURPRISE_SECOND = 3;
+    private final static int SURPRISE_THIRD = 4;
+    private final static int JUMP = 0;
+
+    public FirstLevel() {
+        this.normalBricksFirstRound = NORMAL_FIRST;
+        this.normalBricksSecondRound = NORMAL_SECOND;
+        this.normalBricksThirdRound = NORMAL_THIRD;
+        this.surpriseBricksFirstRound = SURPRISE_FIRST;
+        this.surpriseBricksSecondRound = SURPRISE_SECOND;
+        this.surpriseBricksThirdRound = SURPRISE_THIRD;
+    }
+
+
     @Override
     protected void setFirstRound() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setFirstRound'");
+        this.currentRound = new RoundEasy(JUMP, normalBricksFirstRound, surpriseBricksFirstRound, 600, 400);
     }
 
     @Override
     protected void setSecondRound() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setSecondRound'");
+        this.currentRound = new RoundEasy(JUMP, normalBricksSecondRound, surpriseBricksSecondRound, 600, 400);
     }
 
     @Override
     protected void setThirdRound() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setThirdRound'");
+        this.currentRound = new RoundEasy(JUMP, normalBricksThirdRound, surpriseBricksThirdRound, 600, 400);
     }
     
 }
