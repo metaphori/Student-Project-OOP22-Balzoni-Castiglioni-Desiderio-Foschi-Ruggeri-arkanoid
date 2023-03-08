480a73c49847f3a6a2814290020bc1da7b58dfcf
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-08 17:49:04 +0100

    Implemented RoundEasy.java

--- src/main/java/it/unibo/game/app/model/RoundEasy.java
+++ src/main/java/it/unibo/game/app/model/RoundEasy.java
@@ -1,16 +1,31 @@
 package it.unibo.game.app.model;
 
+import it.unibo.game.app.api.BrickType;
+
 public class RoundEasy extends AbstractRound {
+
+    private int startX;
+    private int startY; 
+    private int endX;
+    private int endY;
+    private int numSurpriseBrick = 0;
 
     public RoundEasy(int jump, int numB, int numS, int bH, int bW) {
         super(jump, numB, numS, bH, bW);
-        //TODO Auto-generated constructor stub
     }
 
     @Override
     public void setPosBrick() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPosBrick'");
+        for(int i = startX; i < endX; i = i + brickH) {
+            for(int j = startY; j < endY; j = j + this.getJump() + brickW) {
+                this.getBrick().add(new NormalBrick(BrickType.NORMAL, j, i, 1));
+            }
+        }
+        while (this.numSurpriseBrick <= this.getNumSur()) {
+            if (this.setBrickSurprise()) {
+                this.numSurpriseBrick++;
+            }
+        }
     }
     
 }
