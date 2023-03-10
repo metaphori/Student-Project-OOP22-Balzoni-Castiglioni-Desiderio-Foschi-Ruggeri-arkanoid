8a1d37796f7d5431bebf053b5cc2d50e57528be6
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-10 17:35:40 +0100

    Created GameOver class

---
+++ src/main/java/it/unibo/game/app/model/GameOver.java
@@ -0,0 +1,10 @@
+package it.unibo.game.app.model;
+
+public class GameOver {
+    
+    protected AbstractRound round;
+
+    public GameOver(AbstractRound round) {
+        this.round = round;
+    }    
+}
