5eafbe8524adba5bfa0882ab0268a8a2d13ed2ed
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-10 14:45:48 +0100

    Added lives variable and methods to modify it into Level class

--- src/main/java/it/unibo/game/app/model/Level.java
+++ src/main/java/it/unibo/game/app/model/Level.java
@@ -6,6 +6,9 @@ import it.unibo.game.Pair;
 
 public abstract class Level {
 
+    private final static int INITIAL_LIVES=3;
+    protected int lives = INITIAL_LIVES;
+
     /*Definire delle costanti per ogni livello che definiscno queste variabili */
     protected int normalBricksFirstRound;
     protected int surpriseBricksFirstRound;
@@ -21,11 +24,17 @@ public abstract class Level {
 
     protected Pair<Integer,Integer> frameSize;
 
-
     /*Posizionano gli oggetti (pad,pallina e blocchi) all'interno di ciascun round*/
     protected abstract void setFirstRound();
     protected abstract void setSecondRound();
     protected abstract void setThirdRound();
+
+    protected int increaseLife() {
+        return this.lives++;
+    }
+    protected int decreaseLife() {
+        return this.lives--;
+    }
     
     public AbstractRound getRound() {
         return this.currentRound;
