0624188986800c9684d6f6bbe6a511da7b9998cf
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-10 18:37:47 +0100

    created the class that stores and manages the score

---
+++ src/main/java/it/unibo/game/app/model/Score.java
@@ -0,0 +1,25 @@
+package it.unibo.game.app.model;
+
+public class Score {
+    private static final int ONE_COLLISION_POINTS = 100;
+    private int points ;
+    private int  score ;
+
+    public Score(){
+        resetPoints();
+        this.score = 0;
+    }
+
+    public Integer getScore (){
+        return this.score;
+    }
+
+    public void increaseScore(){
+        score = score + points;
+        points = points * 2;
+    }
+    
+    public void resetPoints (){
+        this.points = ONE_COLLISION_POINTS;
+    }
+}
