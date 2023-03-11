15ada83fe793a6220ae82f9eba6c8c1233804853
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-11 14:39:43 +0100

    Completed the update method in BrickView, UpdateBallView and UpdatePadView

--- src/main/java/it/unibo/game/app/view/UpdatePadView.java
+++ src/main/java/it/unibo/game/app/view/UpdatePadView.java
@@ -1,12 +1,15 @@
 package it.unibo.game.app.view;
 
+import java.awt.Graphics;
+
+import it.unibo.game.Pair;
 import it.unibo.game.app.api.UpdateView;
 
 public class UpdatePadView implements UpdateView {
 
     @Override
-    public void update() {
-        // TODO Auto-generated method stub
+    public void update(Graphics g, Pair<Integer, Integer> dimension, Pair<Integer, Integer> pos, GameView view) {
+       view.drawPad(pos, dimension, g);
     }
     
 }
