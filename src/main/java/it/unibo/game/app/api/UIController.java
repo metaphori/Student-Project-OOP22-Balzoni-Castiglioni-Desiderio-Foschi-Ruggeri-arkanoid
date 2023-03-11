9bafd18230940d6958d6047ecf0eca576615a884
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-11 21:24:37 +0100

    add method that UIController will call to get from Controller some information of the objects of the game

--- src/main/java/it/unibo/game/app/api/UIController.java
+++ src/main/java/it/unibo/game/app/api/UIController.java
@@ -3,9 +3,26 @@ package it.unibo.game.app.api;
 import java.util.Map;
 
 import it.unibo.game.Pair;
+import java.util.*;
+import it.unibo.game.app.model.*;
 
 public interface UIController {
-    enum PAGES {START_MENU, PAUSE_MENU, GAME, TOP_10};
+    enum PAGES {
+        START_MENU("START MENU"), 
+        PAUSE_MENU("PAUSE MENU"), 
+        GAME("ARKANOID"), 
+        TOP_10("TOP FIVE");
+
+        String name;
+
+        PAGES(String name){
+            this.name=name;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+    };
 
     void initialView();
     void pauseMenu();
@@ -14,4 +31,12 @@ public interface UIController {
     void level(int numLevel);
     Map<Pair<Integer, Integer>, Integer> getList();
     Pair<Integer,Integer> getDimension();
+    Pair<Integer,Integer> getDimensionBrick();
+    Pair<Integer,Integer> getBall();
+    Pair<Integer,Integer> getPad();
+    //aggiunto
+    void changePosPad(Pair<Integer,Integer> pos);
+    int getPadWight();
+    int getPadHeight();
+    double getRBall();
 }
