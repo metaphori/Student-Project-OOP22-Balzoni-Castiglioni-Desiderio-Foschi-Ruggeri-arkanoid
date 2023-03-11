06e7c3d4df3d8446ec90b3abeb138de2e4609f9d
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-11 11:28:26 +0100

    Initialized class that updates pad graphics

---
+++ src/main/java/it/unibo/game/app/view/UpdatePadView.java
@@ -0,0 +1,13 @@
+package it.unibo.game.app.view;
+
+import it.unibo.game.app.api.UpdateView;
+
+public class UpdatePadView implements UpdateView {
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+    }
+    
+}
+
