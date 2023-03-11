852e57047617dd987e98f9d1b8b17dbcec2c6600
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-11 11:30:25 +0100

    Initialized class that updates bricks graphics

---
+++ src/main/java/it/unibo/game/app/view/BrickView.java
@@ -0,0 +1,11 @@
+package it.unibo.game.app.view;
+
+import java.util.Map;
+
+import it.unibo.game.Pair;
+
+public class BrickView {
+    public void update (Map<Pair<Integer, Integer>, Integer> bricks){
+        //ridisegna brick
+    }
+}
