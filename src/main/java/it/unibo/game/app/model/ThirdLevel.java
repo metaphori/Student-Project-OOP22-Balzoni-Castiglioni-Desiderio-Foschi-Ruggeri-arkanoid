32ee0cc030abc6151b4ebf39c801abf5c14a9d25
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-11 13:48:30 +0100

    create class ThirdLevel

---
+++ src/main/java/it/unibo/game/app/model/ThirdLevel.java
@@ -0,0 +1,24 @@
+package it.unibo.game.app.model;
+
+public class ThirdLevel extends Level {
+
+    private AbstractRound round;
+
+    @Override
+    protected void setFirstRound() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected void setSecondRound() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected void setThirdRound() {
+        // TODO Auto-generated method stub
+    }
+
+    
+    
+}
