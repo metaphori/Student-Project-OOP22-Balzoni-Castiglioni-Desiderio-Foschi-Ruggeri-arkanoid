3dbf9cc66f6aafe95cf96c670b0ec04d5f9010f3
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-11 11:29:22 +0100

    Initialized class that updates ball graphics

---
+++ src/main/java/it/unibo/game/app/view/UpdateBallView.java
@@ -0,0 +1,10 @@
+package it.unibo.game.app.view;
+
+import it.unibo.game.app.api.UpdateView;
+
+public class UpdateBallView implements UpdateView {
+    @Override
+    public void update (){
+        //metodo per ridisegnare palla
+    }
+}
