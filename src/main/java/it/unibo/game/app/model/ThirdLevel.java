bb47aee17fe3f16e5cfc72eebfef2f5c422dc015
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-11 13:54:01 +0100

    implemented method of class ThirdLevel: decided how many bricks of each type are present in each round of this level

--- src/main/java/it/unibo/game/app/model/ThirdLevel.java
+++ src/main/java/it/unibo/game/app/model/ThirdLevel.java
@@ -7,16 +7,19 @@ public class ThirdLevel extends Level {
     @Override
     protected void setFirstRound() {
         // TODO Auto-generated method stub
+        this.round=new RoundDifficult(1,22,4,50,25,2);
     }
 
     @Override
     protected void setSecondRound() {
         // TODO Auto-generated method stub
+        this.round=new RoundDifficult(1,22,8,50,25,6);
     }
 
     @Override
     protected void setThirdRound() {
         // TODO Auto-generated method stub
+        this.round=new RoundDifficult(1,26,10,50,25,9);
     }
 
     
