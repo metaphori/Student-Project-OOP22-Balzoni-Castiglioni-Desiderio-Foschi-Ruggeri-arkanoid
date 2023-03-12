570e895202dc061e865d81a35981cde0cb4174f9
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-12 20:31:42 +0100

    Created a new enum, Side,  to specify in which direction the ball collides

--- src/main/java/it/unibo/game/app/api/BoundingBox.java
+++ src/main/java/it/unibo/game/app/api/BoundingBox.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import it.unibo.game.Pair;
 
 public interface BoundingBox {
+    enum Side {UP_DOWN, LEFT_RIGHT};
     enum Corner {LEFT_DOWN , LEFT_UP, RIGHT_DOWN, RIGHT_UP};
     Map<Corner, Pair<Integer, Integer>> getBox();
     Boolean collideWith(BoundingBox b);
