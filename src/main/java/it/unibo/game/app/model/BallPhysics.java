22661e71dd2f7b70fd8f42b0353780fd0f2b9e45
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-12 20:37:28 +0100

    combined the two methods that changed direction vertically and horizontally into a single method (changeDirection)

--- src/main/java/it/unibo/game/app/model/BallPhysics.java
+++ src/main/java/it/unibo/game/app/model/BallPhysics.java
@@ -1,22 +1,28 @@
 package it.unibo.game.app.model;
 
+import java.util.Optional;
+
+import it.unibo.game.app.api.BoundingBox.Side;
+
 public class BallPhysics {
     
     private Direction d = new Direction();
 
-    public void changeDirectionVertical(){
-        if(this.d.isDirectionUp()){
-            this.d.setDirectionDown();
-        }else{
-            this.d.setDirectionUp();
+    public Direction changeDirection(Optional<Side> side){
+        if(side == Optional.of(Side.UP_DOWN)){
+            if(this.d.isDirectionUp()){
+                this.d.setDirectionDown();
+            }else{
+                this.d.setDirectionUp();
+            }
+        }else if(side == Optional.of(Side.LEFT_RIGHT)){
+            if(this.d.isDirectionLeft()){
+                this.d.setDirectionRight();
+            }else{
+                this.d.setDirectionLeft();
+            }
         }
+        return this.d;
     }
-
-    public void changeDirectionHorizontal(){
-        if(this.d.isDirectionLeft()){
-            this.d.setDirectionRight();
-        }else{
-            this.d.setDirectionLeft();
-        }
-    }
+ 
 }
