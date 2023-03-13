bf88c4c5e8c72d116a53ee829303c9ccbc876af5
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-13 21:06:02 +0100

    Implemented JPanel for GameOver view

--- src/main/java/it/unibo/game/app/view/GameOver.java
+++ src/main/java/it/unibo/game/app/view/GameOver.java
@@ -1,5 +1,40 @@
 package it.unibo.game.app.view;
 
-public class GameOver {
+import java.awt.*;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class GameOver extends JPanel{
     
+    private UIControllerImpl uiControllerImpl;
+
+    public GameOver(UIControllerImpl uiCtrl) {
+        this.uiControllerImpl = uiCtrl;
+        JLabel gameOver = new JLabel("GAME OVER");
+        JButton tryAgain = new CustomBtn(30,"Try Again");
+        JButton quit = new CustomBtn(30,"Quit");
+        JButton menu = new CustomBtn(30,"Start Menù");
+        JPanel title = new JPanel();
+        JPanel buttons = new JPanel();
+
+        this.setLayout(new BorderLayout());
+        buttons.setLayout(new GridLayout(3,1,0,1));
+        gameOver.setFont(new Font("Serif",  Font.BOLD, 60));
+        gameOver.setForeground(Color.WHITE);
+
+        title.add(gameOver);
+        buttons.add(tryAgain);
+        buttons.add(menu);
+        buttons.add(quit);
+        title.setBackground(Color.BLACK);
+        buttons.setBackground(Color.BLACK);
+        this.setBackground(Color.BLACK);
+
+        this.add(title, BorderLayout.NORTH);
+        this.add(buttons, BorderLayout.SOUTH);
+
+    }
 }
