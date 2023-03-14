db7e5d5eeedd8ead21732951d6c8dd3a2e0cb1f8
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-14 12:11:35 +0100

    Created and implemented Victory view

---
+++ src/main/java/it/unibo/game/app/view/jswing/impleentation/Victory.java
@@ -0,0 +1,10 @@
+package it.unibo.game.app.view.jswing.impleentation;
+
+public class Victory extends AbstractView{
+
+    public Victory(UIControllerImpl uiCtrl) {
+        super(uiCtrl);
+        this.titleLabel.setText("YOU WIN");
+    }
+        
+}
