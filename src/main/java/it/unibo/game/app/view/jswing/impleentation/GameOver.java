c6c63160995f3d041620ef244541851bb3062f4c
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-14 12:06:29 +0100

    Update GameOver view extending from AbstractView

--- src/main/java/it/unibo/game/app/view/jswing/impleentation/GameOver.java
+++ src/main/java/it/unibo/game/app/view/jswing/impleentation/GameOver.java
@@ -1,68 +1,9 @@
 package it.unibo.game.app.view.jswing.impleentation;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+public class GameOver extends AbstractView{
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-
-public class GameOver extends JPanel{
-    
-    private UIControllerImpl uiControllerImpl;
-
-    public GameOver(UIControllerImpl uiCtrl) {
-        this.uiControllerImpl = uiCtrl;
-        JLabel gameOver = new JLabel("GAME OVER");
-        JButton tryAgain = new CustomBtn(30,"Try Again");
-        JButton quit = new CustomBtn(30,"Quit");
-        JButton menu = new CustomBtn(30,"Start Menù");
-        JPanel title = new JPanel();
-        JPanel buttons = new JPanel();
-
-        this.setLayout(new BorderLayout());
-        buttons.setLayout(new GridLayout(3,1,0,1));
-        gameOver.setFont(new Font("Serif",  Font.BOLD, 60));
-        gameOver.setForeground(Color.WHITE);
-
-        title.add(gameOver);
-        buttons.add(tryAgain);
-        buttons.add(menu);
-        buttons.add(quit);
-        title.setBackground(Color.BLACK);
-        buttons.setBackground(Color.BLACK);
-        this.setBackground(Color.BLACK);
-
-        this.add(title, BorderLayout.NORTH);
-        this.add(buttons, BorderLayout.SOUTH);
-
-        tryAgain.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-            }
-            
-        });
-        menu.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-            }
-        });
-        quit.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-            }
-        });
-
+    public GameOver(UIControllerImpl uiController) {
+        super(uiController);
+        this.titleLabel.setText("GAME OVER");
     }
 }
