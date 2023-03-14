92f817fca0d7a4baed5654c54968dda79062a99a
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-14 18:23:24 +0100

    the changeDirection() method is invoked inside the methods of the Collision class and isCollide returns the index of the block to be deleted

--- src/main/java/it/unibo/game/app/model/Collision.java
+++ src/main/java/it/unibo/game/app/model/Collision.java
@@ -2,6 +2,8 @@ package it.unibo.game.app.model;
 
 import java.util.Optional;
 
+import javax.print.attribute.standard.Sides;
+
 import it.unibo.game.app.api.BoundingBox.Corner;
 import it.unibo.game.app.api.BoundingBox.Side;
 //devi trovare il modo per comunicare dov'è avvenuta la collisione
@@ -14,32 +16,34 @@ public class Collision {
         this.physics = physics;
     
     }
-    public Optional<Side> isCollideWithEdges(Ball b, int height, int width){
+    public void CollideWithEdges(Ball b, int height, int width){
         var ballBox = new BoundingBoxImpl(b);
+        this.physics = new BallPhysics(b.getDir());
         if(ballBox.getBox().get(Corner.LEFT_DOWN).getY() == 0 ||ballBox.getBox().get(Corner.RIGHT_DOWN).getY() == width-1){
-            return Optional.of(Side.LEFT_RIGHT);
-        }else if(ballBox.getBox().get(Corner.LEFT_UP).getX()==0 /* || ballBox.getBox().get(Corner.RIGHT_DOWN).getX() == height-1 */){
-            return Optional.of(Side.UP_DOWN);
+            physics.changeDirection(Side.LEFT_RIGHT);
+        }else if(ballBox.getBox().get(Corner.LEFT_UP).getX()==0 ){
+            physics.changeDirection(Side.UP_DOWN);
         }
-        //}  non so se va gestito nel game over
-        return Optional.empty();
-    }
 
-    public boolean isCollideWithBrick(Ball b){
+    public Optional<Integer> isCollideWithBrick(Ball b){
         var ballBox = new BoundingBoxImpl(b);
         for (NormalBrick obj : level.getRound().getBrick()) {
             var box = new BoundingBoxImpl(obj);
-            if(ballBox.collideWith(box)){
-                return true;
+            if(ballBox.collideWith(box).isPresent()){
+                physics.changeDirection(ballBox.collideWith(box).get());
+                return Optional.of(level.getRound().getBrick().indexOf(obj));
             }
         }
-        return false;
+        return Optional.empty();
     }
 
     public boolean isCollideWithPad (Ball b, Pad p){
         var ballBox = new BoundingBoxImpl(b);
         var padBox = new BoundingBoxImpl(p);
-
-        return ballBox.collideWith(padBox);
+        if(ballBox.collideWith(padBox).isPresent()) {
+            physics.changeDirection(Side.UP_DOWN);
+            return true;
+        }
+        return false;
     }
 }
