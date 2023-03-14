e8ae7608290807b7fffc8c33becae17dc334ec19
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-14 20:59:01 +0100

    fix problems

--- src/main/java/it/unibo/game/app/model/ThirdLevel.java
+++ src/main/java/it/unibo/game/app/model/ThirdLevel.java
@@ -12,21 +12,21 @@ public class ThirdLevel extends AbstractLevel {
     }
 
     @Override
-    protected void setFirstRound() {
+    public void setFirstRound() {
         // TODO Auto-generated method stub
-        super.currentRound=new RoundDifficult(1,22,4,new SizeCalculation(fSize.getX(),fSize.getY(),6,8),2);
+        super.currentRound=new RoundDifficult(1,22,4,new SizeCalculation(fSize.getX(),fSize.getY(),6,8,0),2);
     }
 
     @Override
-    protected void setSecondRound() {
+    public void setSecondRound() {
         // TODO Auto-generated method stub
-        this.currentRound=new RoundDifficult(1,22,8,new SizeCalculation(fSize.getX(),fSize.getY(),7,8),6);
+        this.currentRound=new RoundDifficult(1,22,8,new SizeCalculation(fSize.getX(),fSize.getY(),7,8,0),6);
     }
 
     @Override
-    protected void setThirdRound() {
+    public void setThirdRound() {
         // TODO Auto-generated method stub
-        this.currentRound=new RoundDifficult(1,26,10,new SizeCalculation(fSize.getX(),fSize.getY(),8,9),9);
+        this.currentRound=new RoundDifficult(1,26,10,new SizeCalculation(fSize.getX(),fSize.getY(),8,9,0),9);
     }
 
     
