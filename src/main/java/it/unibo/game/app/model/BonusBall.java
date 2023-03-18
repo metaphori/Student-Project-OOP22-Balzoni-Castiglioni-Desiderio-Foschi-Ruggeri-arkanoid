9aabe81f1938ea5caff84f9abcf7597783a11996
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-18 09:58:36 +0100

    Added constructor in BonusBall

--- src/main/java/it/unibo/game/app/model/BonusBall.java
+++ src/main/java/it/unibo/game/app/model/BonusBall.java
@@ -1,9 +1,22 @@
 package it.unibo.game.app.model;
 
+import it.unibo.game.Pair;
 import it.unibo.game.app.api.BallPhysics;
 import it.unibo.game.app.api.MovingObject;
 
 public class BonusBall extends GameObjectImpl implements MovingObject{
+
+    private BallPhysics bonusBallPhysics;
+    private Pair<Integer,Integer> coordinates;
+    private int height;
+    private int width;
+
+    public BonusBall(Pair<Integer,Integer> coord, int h, int w) {
+        this.coordinates = coord;
+        this.height = h;
+        this.width = w;
+    }
+
 
     @Override
     public BallPhysics getPhysics() {
