a9d64d02ab01a0b0adef702167a29072ca0e0942
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-18 09:48:22 +0100

    Created BonusBall class

---
+++ src/main/java/it/unibo/game/app/model/BonusBall.java
@@ -0,0 +1,14 @@
+package it.unibo.game.app.model;
+
+import it.unibo.game.app.api.BallPhysics;
+import it.unibo.game.app.api.MovingObject;
+
+public class BonusBall extends GameObjectImpl implements MovingObject{
+
+    @Override
+    public BallPhysics getPhysics() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPhysics'");
+    }
+    
+}
