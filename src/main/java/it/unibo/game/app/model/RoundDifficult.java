968214188c274f7375d93bf6d1198ea2669d6b62
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2023-03-18 12:35:12 +0100

    Fixed SizeCalculation dependencies

--- src/main/java/it/unibo/game/app/model/RoundDifficult.java
+++ src/main/java/it/unibo/game/app/model/RoundDifficult.java
@@ -28,7 +28,7 @@ public class RoundDifficult extends AbstractRound {
         for(int i=getSizeCalc().getStart().getX(); super.brick.size()<(this.obstacles+this.getNumBrick()+this.getNumSur());i=i+getSizeCalc().getBrickDim().getX()){
             num++;
             lines=0;
-            for(int j=getSizeCalc().getFrameSize().getY()/2-(num)*(getSizeCalc().getBrickDim().getY()/2)-10; lines<num; j=j+getSizeCalc().getBrickDim().getY()){
+            for(int j=getSizeCalc().getWorldSize().getY()/2-(num)*(getSizeCalc().getBrickDim().getY()/2)-10; lines<num; j=j+getSizeCalc().getBrickDim().getY()){
                 NormalBrick b = new NormalBrick(BrickType.NORMAL, getSizeCalc().getBrickDim().getY(), getSizeCalc().getBrickDim().getX(),1);
                 b.setPos(new Pair<>(j,i));
                 super.brick.add(b);
