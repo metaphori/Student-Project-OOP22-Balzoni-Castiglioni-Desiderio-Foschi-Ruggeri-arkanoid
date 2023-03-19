f611a8f4d3420be8fb20522facb187745c6b8bf7
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-19 17:24:14 +0100

    change occurrences of NormalBrick with Brick

--- src/main/java/it/unibo/game/app/model/round/AbstractRound.java
+++ src/main/java/it/unibo/game/app/model/round/AbstractRound.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 import it.unibo.game.Pair;
+import it.unibo.game.app.api.Brick;
 import it.unibo.game.app.api.BrickType;
 import it.unibo.game.app.api.GameObject;
 import it.unibo.game.app.api.Round;
@@ -19,7 +20,7 @@ public abstract class AbstractRound implements Round {
     private int jump; /*Serve per indicate quanti blocchi saltare se vogliamo fare le colonne */
     private int numBrick;
     private int numSurprise;
-    protected List<NormalBrick> brick = new ArrayList<>();
+    protected List<Brick> brick = new ArrayList<>();
     private Ball ball = new Ball();
     private Pad pad;
     private SizeCalculation sizeC;
@@ -30,8 +31,8 @@ public abstract class AbstractRound implements Round {
         this.numBrick = numB;
         this.numSurprise = numS;
         this.sizeC = size;
-        pad = new Pad(size.getWorldSize());
-        ball.setR(size.getWorldSize().getY()/15);
+        pad = new Pad(SizeCalculation.getWorldSize());
+        ball.setR(SizeCalculation.getWorldSize().getY()/15);
         ball.setPos(new Pair<>(pad.getPos().getX(),pad.getPos().getY()-ball.getR()));
     }
 
@@ -51,7 +52,7 @@ public abstract class AbstractRound implements Round {
         return this.numSurprise;
     }
 
-    public List<NormalBrick> getBrick () {
+    public List<Brick> getBrick () {
         return this.brick; 
     }
 
