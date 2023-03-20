6cda0f148bd033fd4650c84f165d29fac349d2a6
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2023-03-20 14:12:57 +0100

    Added method to call the ball position update method in the Model class

--- src/main/java/it/unibo/game/app/controller/ControllerImpl.java
+++ src/main/java/it/unibo/game/app/controller/ControllerImpl.java
@@ -19,13 +19,11 @@ public class ControllerImpl implements AppController{
 
     @Override
     public void play() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'play'");
+        this.gameEngine.resume();
     }
 
     @Override
     public void onPause() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'onPause'");
     }
 
@@ -58,7 +56,6 @@ public class ControllerImpl implements AppController{
     }
     
     @Override
-    //non va bene deve prendere le informazioni da SizeCaplulator
     public Pair<Double, Double> getWorldDimension() {
        return SizeCalculation.getWorldSize();
     }
@@ -70,13 +67,11 @@ public class ControllerImpl implements AppController{
 
     @Override
     public Pair<Double, Double> getBall() {
-        // TODO Auto-generated method stub
         return this.model.getBall();
     }
 
     @Override
     public Pair<Double, Double> getPad() {
-        // TODO Auto-generated method stub
         return this.model.getPad();
     }
 
@@ -120,5 +115,9 @@ public class ControllerImpl implements AppController{
     @Override
     public void setGameEngine() {
         this.gameEngine = new GameEngine(this);
+    }
+
+    public void update(long dt) {
+        this.model.update(dt);
     }
 }
