87343e75bf337964370969d30d600c95d6ce0fdb
Margherita <margherita.balzoni@studio.unibo.it>
2023-03-21 18:16:00 +0100

    Fixed BoundingBoxImpl class

--- src/main/java/it/unibo/game/app/model/BoundingBoxImpl.java
+++ src/main/java/it/unibo/game/app/model/BoundingBoxImpl.java
@@ -16,42 +16,36 @@ public class BoundingBoxImpl implements BoundingBox {
     private Map<Corner, Pair<Double,Double>> corners = new HashMap<>();
   
     public BoundingBoxImpl(Ball b){
-        var radius = b.getR();
-        var centre = b.getPos();
-        this.corners.put(Corner.LEFT_DOWN, new Pair<Double,Double>(centre.getX()+radius,centre.getY()-radius ));
-        this.corners.put(Corner.LEFT_UP,new Pair<Double,Double>(centre.getX()-radius,centre.getY()-radius ) );
-        this.corners.put(Corner.RIGHT_DOWN, new Pair<Double,Double>(centre.getX()+radius,centre.getY()+radius ));
-        this.corners.put(Corner.RIGHT_UP,new Pair<Double,Double>(centre.getX()-radius,centre.getY()+radius ));
-  
+        setRectBox(b.getR()*2, b.getR()*2, b.getPos());
     }
     public BoundingBoxImpl(Brick b){
-       setRectBox(b.getBrickH(), b.getBrickW(), b.getPos());
+       setRectBox(b.getBrickW(), b.getBrickH(), b.getPos());
     }
 
     public BoundingBoxImpl (Pad p){
-       setRectBox(p.getHight(), p.getWidth(), p.getPos());
+       setRectBox(p.getWidth(), p.getHight(), p.getPos());
     }
     private void setRectBox (Double double1, Double double2, Pair<Double, Double> pair){
-        this.corners.put(Corner.LEFT_DOWN,new Pair<Double,Double>( pair.getX()+double1, pair.getY() ) );
+        this.corners.put(Corner.LEFT_DOWN,new Pair<Double,Double>( pair.getX(), pair.getY()+double2 ) );
         this.corners.put(Corner.LEFT_UP, pair );
         this.corners.put(Corner.RIGHT_DOWN,new Pair<Double,Double>(pair.getX() + double1, pair.getY() + double2) );
-        this.corners.put(Corner.RIGHT_UP,new Pair<Double,Double> (pair.getX(), pair.getY()+double2));
+        this.corners.put(Corner.RIGHT_UP,new Pair<Double,Double> (pair.getX()+double1, pair.getY()));
       
     }
     @Override
     public Optional<Side> collideWith(BoundingBox b) {
-        if((this.corners.get(Corner.LEFT_UP).getX() <= b.getBox().get(Corner.LEFT_DOWN).getX() 
-            || this.corners.get(Corner.LEFT_DOWN).getX() >= b.getBox().get(Corner.LEFT_UP).getX())
+        if((this.corners.get(Corner.LEFT_UP).getX() == b.getBox().get(Corner.LEFT_DOWN).getX() 
+            || this.corners.get(Corner.LEFT_DOWN).getX() == b.getBox().get(Corner.LEFT_UP).getX())
             && (this.corners.get(Corner.LEFT_UP).getY()<= b.getBox().get(Corner.RIGHT_DOWN).getY() 
             && this.corners.get(Corner.RIGHT_UP).getY()>= b.getBox().get(Corner.LEFT_DOWN).getY())){
 
-                return Optional.of(Side.UP_DOWN);
+                return Optional.of(Side.LEFT_RIGHT);
 
-            }else if((this.corners.get(Corner.RIGHT_DOWN).getY() >= b.getBox().get(Corner.LEFT_UP).getY() 
-                    || this.corners.get(Corner.LEFT_DOWN).getY() <= b.getBox().get(Corner.RIGHT_UP).getY())
+            }else if((this.corners.get(Corner.RIGHT_DOWN).getY() == b.getBox().get(Corner.LEFT_UP).getY() 
+                    || this.corners.get(Corner.LEFT_DOWN).getY() == b.getBox().get(Corner.RIGHT_UP).getY())
                     && (this.corners.get(Corner.RIGHT_DOWN).getX() >= b.getBox().get(Corner.LEFT_UP).getX() 
                     && this.corners.get(Corner.RIGHT_UP).getX() <= b.getBox().get(Corner.LEFT_DOWN).getX())){
-                        return Optional.of(Side.LEFT_RIGHT);
+                        return Optional.of(Side.UP_DOWN);
                     }
    
        return Optional.empty();
