114e197b29f23724d30242ef07765359edc54ef0
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-21 11:25:53 +0100

    Deleted Surprise interface and renamed SurpriseImpl to Surprise

---
+++ src/main/java/it/unibo/game/app/model/Surprise.java
@@ -0,0 +1,65 @@
+package it.unibo.game.app.model;
+
+import it.unibo.game.app.api.Level;
+
+public class Surprise {
+
+    private Level level;
+
+    public Surprise(Level level) {
+        this.level = level;
+    }
+
+    //simone
+    public void extraLife() {        
+    }
+
+    //simone
+    public void explosiveBomb() {
+    }
+
+    //edoardo
+    public void deleteRandomBricks() {
+    }
+
+    //edoardo
+    public void reduceSizePad() {
+    }
+
+    //edoardo
+    public void enlargeSizePad() {
+    }
+
+    //virginia
+    public void increaseBallSpeed() {
+    }
+
+    //virginia
+    public void decreaseBallSpeed() {
+    }
+
+    //virginia
+    public void changeObstacles() {
+    }
+
+    //margherita
+    public void increaseScore() {
+    }
+
+    //margherita
+    public void addBalls() {
+    }
+
+    //chiara
+    public void changeRow() {
+    }
+
+    //chiara
+    public void changeHard() {
+    }
+
+    //simone
+    public void chooseSurprise() {
+    }
+    
+}
