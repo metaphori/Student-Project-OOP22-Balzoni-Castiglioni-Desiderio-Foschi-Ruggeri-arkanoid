c25f24a4802e0332da7bd26afce29c2b3ad8422b
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-24 19:14:38 +0100

    number of blocks are costants

--- src/main/java/it/unibo/game/app/model/levels/ThirdLevel.java
+++ src/main/java/it/unibo/game/app/model/levels/ThirdLevel.java
@@ -6,29 +6,38 @@ import it.unibo.game.app.model.round.*;
 
 public class ThirdLevel extends AbstractLevel {
 
-    //private Pair<Double, Double> worldSize;
+    private static final int NORMAL1=6;
+    private static final int SURPRISE1=2;
+    private static final int OBSTACLE1=2;
 
-    public ThirdLevel(Pair<Double, Double> pair){
-        this.worldSize=pair;
+    private static final int NORMAL2=13;
+    private static final int SURPRISE2=4;
+    private static final int OBSTACLE2=4;
+
+    private static final int NORMAL3=16;
+    private static final int SURPRISE3=6;
+    private static final int OBSTACLE3=6;
+
+    public ThirdLevel(){
         this.setFirstRound();
     }
 
     @Override
     public void setFirstRound() {
         // TODO Auto-generated method stub
-        super.currentRound=new RoundDifficult(1,6,2,new SizeCalculation(4,5,0),2);
+        super.currentRound=new RoundDifficult(1,NORMAL1,SURPRISE1,new SizeCalculation(4,5,0),OBSTACLE1);
     }
 
     @Override
     public void setSecondRound() {
         // TODO Auto-generated method stub
-        this.currentRound=new RoundDifficult(1,13,4,new SizeCalculation(6,7,0),4);
+        this.currentRound=new RoundDifficult(1,NORMAL2,SURPRISE2,new SizeCalculation(6,7,0),OBSTACLE2);
     }
 
     @Override
     public void setThirdRound() {
         // TODO Auto-generated method stub
-        this.currentRound=new RoundDifficult(1,16,6,new SizeCalculation(7,8,0),6);
+        this.currentRound=new RoundDifficult(1,NORMAL3,SURPRISE3,new SizeCalculation(7,8,0),OBSTACLE3);
     }
 
     
