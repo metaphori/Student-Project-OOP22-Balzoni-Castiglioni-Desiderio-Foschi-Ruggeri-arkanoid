ffda8b16289ad162b80cf2a81d9a764f68b4fc09
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2023-03-25 19:07:30 +0100

    Added initialization of score inside ThirdLevel constructor

--- src/main/java/it/unibo/game/app/model/levels/ThirdLevel.java
+++ src/main/java/it/unibo/game/app/model/levels/ThirdLevel.java
@@ -1,6 +1,7 @@
 package it.unibo.game.app.model.levels;
 
 import it.unibo.game.Pair;
+import it.unibo.game.app.model.ScoreImpl;
 import it.unibo.game.app.model.SizeCalculation;
 import it.unibo.game.app.model.round.*;
 
@@ -20,6 +21,7 @@ public class ThirdLevel extends AbstractLevel {
 
     public ThirdLevel(){
         super.levelId=3;
+        super.score = new ScoreImpl();
         this.setFirstRound();
     }
 
