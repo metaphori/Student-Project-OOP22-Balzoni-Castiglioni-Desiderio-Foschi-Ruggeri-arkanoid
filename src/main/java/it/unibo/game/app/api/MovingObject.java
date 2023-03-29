8eb7d53e2a1bfe1159be1d21f9d6c408ba8118d1
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-03-29 23:00:43 +0200

    due to new oraganization refactored interface

--- src/main/java/it/unibo/game/app/api/MovingObject.java
+++ src/main/java/it/unibo/game/app/api/MovingObject.java
@@ -1,6 +1,14 @@
 package it.unibo.game.app.api;
 /*la calsse Speeed sono affari della Virginia */
 
-public interface MovingObject{
-    Physics getPhysics();    
+public interface MovingObject extends GameObject {
+	void setPhysics(Physics phsycs);
+
+	Physics getPhysics();
+
+	void setDimension(Dimension d);
+
+	Speed getSpeed();
+
+	void setSpeed(Speed vel);
 }
