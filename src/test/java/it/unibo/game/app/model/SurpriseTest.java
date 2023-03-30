74f8666119e3284b3d96ec99c9a0e7d724adf381
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-03-30 13:57:29 +0200

    Implemented testExtraLife to SurpriseTest

--- src/test/java/it/unibo/game/app/model/SurpriseTest.java
+++ src/test/java/it/unibo/game/app/model/SurpriseTest.java
@@ -1,6 +1,31 @@
 package it.unibo.game.app.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.Test;
+
+import it.unibo.game.app.api.AppController;
+import it.unibo.game.app.api.Level;
+import it.unibo.game.app.controller.ControllerImpl;
+import it.unibo.game.app.model.levels.FirstLevel;
+
 public class SurpriseTest {
 	
-	
+	@Test
+    void testExtraLife() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException  {
+        AppController controller = new ControllerImpl();
+        Level level = new FirstLevel(controller.getWorldDimension());
+        Surprise surprise = new Surprise(level);
+
+        Method method = Surprise.class.getDeclaredMethod("extraLife");
+        method.setAccessible(true);
+        
+        int oldLives = level.getLife();
+        method.invoke(surprise);
+        int newLives = level.getLife();
+        assertEquals(oldLives + 1, newLives);
+    }
 }
