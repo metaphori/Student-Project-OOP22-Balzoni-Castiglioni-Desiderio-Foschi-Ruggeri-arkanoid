013c277ec95d325f56831efeac7d2f161c89d38b
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-30 21:23:34 +0200

    fix check style

--- src/main/java/it/unibo/game/app/model/levels/AbstractLevel.java
+++ src/main/java/it/unibo/game/app/model/levels/AbstractLevel.java
@@ -1,81 +1,139 @@
 package it.unibo.game.app.model.levels;
 
-import it.unibo.game.Pair;
 import it.unibo.game.app.api.Brick;
 import it.unibo.game.app.api.Level;
 import it.unibo.game.app.api.Round;
 import it.unibo.game.app.api.Score;
-import it.unibo.game.app.model.SizeCalculation;
+import it.unibo.game.app.model.ScoreImpl;
 
-
-/*Questa classe astratta dichiara variabili e definisce metodi in comune ai vari livelli */
-
+/**
+ * Questa classe astratta dichiara variabili e definisce metodi in comune ai
+ * vari livelli.
+ */
 public abstract class AbstractLevel implements Level {
 
-    private final static int INITIAL_LIVES=3;
-    protected int lives = INITIAL_LIVES;
+	private final static int INITIAL_LIVES = 3;
+	private int lives = INITIAL_LIVES;
+	private Round currentRound;
+	private Score score;
+	private int numRoundPassed = 0;
+	private int levelId;
+	private Brick lastSurpriseBrick;
+	private int indexLastSurprise;
 
-    /*Definire delle costanti per ogni livello che definiscno queste variabili */
-    protected int normalBricksFirstRound;
-    protected int surpriseBricksFirstRound;
+	/**
+	 * constructor of this class.
+	 * 
+	 * @param levelId represents level identifier
+	 */
+	public AbstractLevel(final int levelId) {
+		this.levelId = levelId;
+		this.score = new ScoreImpl();
+	}
 
-    protected int normalBricksSecondRound;
-    protected int surpriseBricksSecondRound;
+	/*
+	 * Posizionano gli oggetti (pad,pallina e blocchi) all'interno di ciascun round
+	 */
+	public abstract void setFirstRound();
 
-    protected int normalBricksThirdRound;
-    protected int surpriseBricksThirdRound;
+	public abstract void setSecondRound();
 
-    protected Round currentRound;
-    protected SizeCalculation sizeCalc;
-    protected Score score;
+	public abstract void setThirdRound();
 
-    protected Pair<Double, Double> worldSize;
-    protected int numRoundPassed = 0;
-    protected int levelId;
-    protected Brick lastSurpriseBrick;
-    protected int indexLastSurprise;
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void increaseLife() {
+		this.lives++;
+	}
 
-    /*Posizionano gli oggetti (pad,pallina e blocchi) all'interno di ciascun round*/
-    public abstract void setFirstRound();
-    public abstract void setSecondRound();
-    public abstract void setThirdRound();
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void decreaseLife() {
+		this.lives--;
+	}
 
-    public int increaseLife() {
-        return this.lives++;
-    }
-    public int decreaseLife() {
-        return this.lives--;
-    }
-    public boolean isAlive() {
-        return this.lives > 0 ? true : false;
-    }
-    
-    public Round getRound() {
-        return this.currentRound;
-    }
-    public int getNumRoundPassed() {
-        return this.numRoundPassed;
-    }
-    public void increaseRound() {
-        ++this.numRoundPassed;
-    }
-    public int getLife() {
-        return this.lives;
-    }
-    public int getId(){
-        return this.levelId;
-    }
-    public Score getScore(){
-        return this.score;
-    }
-    public void setLastSurpriseBrick(Brick surBrick, int i) {
-        this.lastSurpriseBrick = surBrick;
-        this.indexLastSurprise = i;
-    }
-    public Brick getLastSurpriseBrick() {
-        return this.lastSurpriseBrick;
-    }
-    public int getIndex() {
-        return this.indexLastSurprise;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isAlive() {
+		return this.lives > 0 ? true : false;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Round getRound() {
+		return this.currentRound;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setRound(final Round r) {
+		this.currentRound = r;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getNumRoundPassed() {
+		return this.numRoundPassed;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void increaseRound() {
+		++this.numRoundPassed;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getLife() {
+		return this.lives;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getId() {
+		return this.levelId;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Score getScore() {
+		return this.score;
+	}
+
+	public void setLastSurpriseBrick(final Brick surBrick, final int i) {
+		this.lastSurpriseBrick = surBrick;
+		this.indexLastSurprise = i;
+	}
+
+	public int getRoundPassed() {
+		return this.numRoundPassed;
+	}
+
+	public Brick getLastSurpriseBrick() {
+		return this.lastSurpriseBrick;
+	}
+
+	public int getIndex() {
+		return this.indexLastSurprise;
+	}
 }
