cd7077f34e9df1d4fddc6b1e63273ac651a12feb
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-31 17:58:32 +0200

    fix check style class Obstacle

--- src/main/java/it/unibo/game/app/model/brick/Obstacle.java
+++ src/main/java/it/unibo/game/app/model/brick/Obstacle.java
@@ -6,39 +6,65 @@ import it.unibo.game.Pair;
 import it.unibo.game.app.api.BrickType;
 import it.unibo.game.app.api.Dimension;
 
+/**
+ * an obstacle is a brick that is undestroyable.
+ */
 public class Obstacle extends AbstractBrick {
 
-    public Obstacle(BrickType type, Dimension d , Pair<Double,Double> pos) {
-        super(type, d,pos);
-        //TODO Auto-generated constructor stub
-    }
+	/**
+	 * constructor of this class.
+	 * 
+	 * @param type type of brick
+	 * @param d    dimension of brick
+	 * @param pos  position of upper-left corner
+	 */
+	public Obstacle(final BrickType type, final Dimension d,
+			final Pair<Double, Double> pos) {
+		super(type, d, pos);
+		// TODO Auto-generated constructor stub
+	}
 
-    @Override
-    public boolean isDestroyed() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isDestroyed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public void hit() {
-        // TODO Auto-generated method stub
-        
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void hit() {
+		// TODO Auto-generated method stub
 
-    @Override
-    public Optional<Integer> getRes() {
-        // TODO Auto-generated method stub
-        return Optional.empty();
-    }
+	}
 
-    @Override
-    public void increaseRes(int res) {
-        // TODO Auto-generated method stub
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Optional<Integer> getRes() {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
 
-    @Override
-    public void decreaseRes(int res) {
-        // TODO Auto-generated method stub
-    }
-    
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void increaseRes(final int res) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void decreaseRes(final int res) {
+		// TODO Auto-generated method stub
+	}
+
 }
