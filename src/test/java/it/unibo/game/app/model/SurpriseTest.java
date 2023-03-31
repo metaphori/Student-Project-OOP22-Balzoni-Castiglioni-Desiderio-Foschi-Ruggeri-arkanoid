96d9c2f08de2281e66312aa4e2a3eb4ad4db8c12
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2023-03-31 19:36:17 +0200

    Implemented test for addHardRow in SurpriseTest

--- src/test/java/it/unibo/game/app/model/SurpriseTest.java
+++ src/test/java/it/unibo/game/app/model/SurpriseTest.java
@@ -6,10 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.Socket;
 
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,76 +20,116 @@ import java.util.Map;
 import it.unibo.game.app.api.Level;
 import it.unibo.game.app.model.levels.FirstLevel;
 import it.unibo.game.app.model.levels.SecondLevel;
+import it.unibo.game.app.model.levels.ThirdLevel;
 import it.unibo.game.app.api.Brick;
 import it.unibo.game.app.api.BrickType;
 
 public class SurpriseTest {
 
-	@Test
-	void testExtraLife() throws NoSuchMethodException, SecurityException,
-			IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Level level = new FirstLevel();
-		Surprise surprise = new Surprise(level);
+  @Test
+  void testExtraLife() throws NoSuchMethodException, SecurityException,
+      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    Level level = new FirstLevel();
+    Surprise surprise = new Surprise(level);
 
-		Method method = Surprise.class.getDeclaredMethod("extraLife");
-		method.setAccessible(true);
+    Method method = Surprise.class.getDeclaredMethod("extraLife");
+    method.setAccessible(true);
 
-		int oldLives = level.getLife();
-		method.invoke(surprise);
-		int newLives = level.getLife();
-		assertEquals(oldLives + 1, newLives);
-	}
+    int oldLives = level.getLife();
+    method.invoke(surprise);
+    int newLives = level.getLife();
+    assertEquals(oldLives + 1, newLives);
+  }
 
-	@Test
-	void testChangeHard()
-			throws NoSuchMethodException, SecurityException, IllegalAccessException,
-			IllegalArgumentException, InvocationTargetException, InterruptedException {
-		Level level = new SecondLevel();
-		Surprise surprise = new Surprise(level);
+  @Test
+  void testChangeHard()
+      throws NoSuchMethodException, SecurityException, IllegalAccessException,
+      IllegalArgumentException, InvocationTargetException, InterruptedException {
+    Level level = new SecondLevel();
+    Surprise surprise = new Surprise(level);
 
-		Method method = Surprise.class.getDeclaredMethod("changeHard");
-		method.setAccessible(true);
+    Method method = Surprise.class.getDeclaredMethod("changeHard");
+    method.setAccessible(true);
 
-		List<Integer> oldHardIndex = new ArrayList<>();
-		/*
-		 * level.getRound().getBrick().stream().filter(b -> b.getRes().get().equals(2))
-		 * .map(b -> oldHardIndex.add(level.getRound().getBrick().indexOf(b)));
-		 */
-		for (var brick : level.getRound().getBrick()) {
-			if (brick.getRes().get() == 2) {
-				oldHardIndex.add(level.getRound().getBrick().indexOf(brick));
-			}
-		}
-		method.invoke(surprise);
-		for (var i : oldHardIndex) {
-			assertEquals(1, level.getRound().getBrick().get(i).getRes().get());
-		}
-		Thread.sleep(10010);
-		for (var i : oldHardIndex) {
-			assertEquals(2, level.getRound().getBrick().get(i).getRes().get());
-		}
-	}
+    List<Integer> oldHardIndex = new ArrayList<>();
+    /*
+     * level.getRound().getBrick().stream().filter(b -> b.getRes().get().equals(2))
+     * .map(b -> oldHardIndex.add(level.getRound().getBrick().indexOf(b)));
+     */
+    for (var brick : level.getRound().getBrick()) {
+      if (brick.getRes().get() == 2) {
+        oldHardIndex.add(level.getRound().getBrick().indexOf(brick));
+      }
+    }
+    method.invoke(surprise);
+    for (var i : oldHardIndex) {
+      assertEquals(1, level.getRound().getBrick().get(i).getRes().get());
+    }
+    Thread.sleep(10100);
+    for (var i : oldHardIndex) {
+      assertEquals(2, level.getRound().getBrick().get(i).getRes().get());
+    }
+  }
 
-  @Test 
-  void testExplosiveBomb() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+  @Test
+  void testExplosiveBomb() throws NoSuchMethodException, SecurityException,
+      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
     Level level = new FirstLevel();
     Surprise surprise = new Surprise(level);
     Method method = Surprise.class.getDeclaredMethod("explosiveBomb");
     method.setAccessible(true);
-    Map<Brick,Integer> mappa = new HashMap<>();
+    Map<Brick, Integer> mappa = new HashMap<>();
     int oldSize = level.getRound().getBrick().size();
 
-    for(Brick brick : level.getRound().getBrick()) {
-      if(brick.getType().equals(BrickType.SURPRISE)) {
+    for (Brick brick : level.getRound().getBrick()) {
+      if (brick.getType().equals(BrickType.SURPRISE)) {
         mappa.put(brick, level.getRound().getBrick().indexOf(brick));
       }
-    }			
-    for (Map.Entry<Brick,Integer> element : mappa.entrySet()) {
+    }
+    for (Map.Entry<Brick, Integer> element : mappa.entrySet()) {
       level.setLastSurpriseBrick(element.getKey(), element.getValue());
       method.invoke(surprise);
       int newSize = level.getRound().getBrick().size();
-      assertTrue(oldSize == newSize +1 || oldSize == newSize +2 || oldSize == newSize +3 || oldSize == newSize +4);
+      assertTrue(oldSize == newSize + 1 || oldSize == newSize + 2
+          || oldSize == newSize + 3 || oldSize == newSize + 4);
       assertNotEquals(oldSize, newSize);
     }
+  }
+
+  @Test
+  void initTestAddHardRow() throws NoSuchMethodException, SecurityException,
+      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    Level l1 = new FirstLevel();
+    Level l2 = new SecondLevel();
+    testAddHardRow(l1);
+    testAddHardRow(l2);
+  }
+
+  void testAddHardRow(Level l) throws NoSuchMethodException, SecurityException,
+      IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    List<Brick> reverse = new ArrayList<>();
+    Surprise surprise = new Surprise(l);
+    int count = 0;
+    Method method = Surprise.class.getDeclaredMethod("addHardRow");
+    method.setAccessible(true);
+
+    reverse.addAll(l.getRound().getBrick());
+    Collections.reverse(reverse);
+    Double y = reverse.get(reverse.size() - 1).getPos().getY();
+    for (Brick brick : reverse) {
+      if (brick.getPos().getY() == y) {
+        ++count;
+      }
+    }
+    int oldSize = l.getRound().getBrick().size();
+    method.invoke(surprise);
+    if (l.getId() == 2) {
+      assertEquals(oldSize + count + 2, l.getRound().getBrick().size());
+    } else {
+      assertEquals(oldSize + count, l.getRound().getBrick().size());
+    }
+    assertEquals(
+        l.getRound().getBrick().get(l.getRound().getBrick().size() - 1).getPos().getX(),
+        reverse.get(0).getPos().getX());
   }
 }
