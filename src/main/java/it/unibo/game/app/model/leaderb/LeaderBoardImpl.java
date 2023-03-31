371fe45a2aea82c671cf89f1cb3a4b2d335b7014
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-03-31 18:17:32 +0200

    fix check style

--- src/main/java/it/unibo/game/app/model/leaderb/LeaderBoardImpl.java
+++ src/main/java/it/unibo/game/app/model/leaderb/LeaderBoardImpl.java
@@ -21,16 +21,17 @@ import it.unibo.game.Pair;
  */
 public class LeaderBoardImpl implements java.io.Serializable, LeaderBoard {
 
-	private File file = new File("src/main/Filee.txt");
+	private final File file = new File("src/main/Filee.txt");
 	private static final int MAX = 5;
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void updatePoints(final String name, final String passWord, final Integer points, final Integer levelId) {
-		List<User> players = playersFromFile();
-		Optional<User> user = isPresent(name, passWord, players);
+	public void updatePoints(final String name, final String passWord, final Integer points,
+			final Integer levelId) {
+		final List<User> players = playersFromFile();
+		final Optional<User> user = checkUser(name, passWord, players);
 		if (user.isPresent()) {
 			user.get().update(points, levelId);
 		} else {
@@ -48,8 +49,11 @@ public class LeaderBoardImpl implements java.io.Serializable, LeaderBoard {
 	 * @param users    list that contains all players
 	 * @return an optional that contains the User if is present
 	 */
-	private Optional<User> isPresent(final String usr, final String passWord, final List<User> users) {
-		return users.stream().filter(x -> x.getName().equals(usr) && x.getPassWord().equals(passWord)).findFirst();
+	private Optional<User> checkUser(final String usr, final String passWord,
+			final List<User> users) {
+		return users.stream()
+				.filter(x -> x.getName().equals(usr) && x.getPassWord().equals(passWord))
+				.findFirst();
 	}
 
 	/**
@@ -61,15 +65,17 @@ public class LeaderBoardImpl implements java.io.Serializable, LeaderBoard {
 		if (players.size() > MAX) {
 			players = players.subList(0, MAX);
 		}
-		return players.stream().map(x -> new Pair<>(x.getName(), x.getPoints())).collect(Collectors.toList());
+		return players.stream().map(x -> new Pair<>(x.getName(), x.getPoints()))
+				.collect(Collectors.toList());
 	}
 
 	private List<User> playersFromFile() {
 		if (this.file.length() > 0) {
-			try (
-					ObjectInputStream oos = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file.getPath())))) {
+			try (ObjectInputStream oos = new ObjectInputStream(
+					new BufferedInputStream(new FileInputStream(file.getPath())))) {
 				return (List<User>) oos.readObject();
 			} catch (Exception ex) {
+				System.out.println(ex.toString());
 			}
 		}
 		return new ArrayList<>();
@@ -80,18 +86,19 @@ public class LeaderBoardImpl implements java.io.Serializable, LeaderBoard {
 				new BufferedOutputStream(new FileOutputStream(file.getPath())))) {
 			oos.writeObject(users);
 		} catch (Exception ex) {
+			System.out.println(ex.toString());
 		}
 	}
 
 	class User implements java.io.Serializable {
-		private String name;
-		private String password;
-		private Map<Integer, Integer> points = new HashMap<>();
+		private final String name;
+		private final String password;
+		private final Map<Integer, Integer> points = new HashMap<>();
 
 		User(final String name, final String password, final int points, final int levelId) {
 			this.name = name;
 			this.password = password;
-			this.update(points, levelId);
+			this.points.put(levelId, points);
 		}
 
 		public String getName() {
