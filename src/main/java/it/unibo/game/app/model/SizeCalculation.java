c310ab86e1c2e96af956f3e87fff7b4bcdbcf0cc
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2023-04-01 17:44:03 +0200

    Format SizeCalculation

--- src/main/java/it/unibo/game/app/model/SizeCalculation.java
+++ src/main/java/it/unibo/game/app/model/SizeCalculation.java
@@ -3,102 +3,117 @@ package it.unibo.game.app.model;
 import it.unibo.game.Pair;
 import it.unibo.game.app.api.Dimension;
 
+/**
+ * Class that calculates the size of the entity based on the size of the model.
+ */
 public class SizeCalculation {
-	private final static Double WorldHeight = 400d;/* Height of the world inside the model */
-	private final static Double WorldWidth = 300d; /* Width of the world inside the model */
-	private int numBrickCol;
-	private Double startX;
-	private Double startY = 0d;
-	private Double stopX;
-	private Double stopY;
-	private Double brickL;
-	private Double brickH;
+  private static final Double WORLD_HEIGHT = 400d; /*
+                                                    * Height of the world inside the model
+                                                    */
+  private static final Double WORLD_WIDTH = 300d; /* Width of the world inside the model */
+  private static final int DIVIDER_X = 6;
+  private static final double SPACE_Y = 7.5;
+  private static final int NUMCOL = 6;
+  private static final double MUL_X1 = 1.75;
+  private static final double MUL_X2 = 1.5;
+  private static final double DIVEDER_PAD_X = 60;
+  private static final double DIVEDER_BALL = 30;
 
-	/**
-	 * 
-	 * @param numBrickCol number of bricks in a column
-	 * @param numBrickRow number of brick in a row
-	 * @param roundPassed number of rounds passed
-	 */
-	public SizeCalculation(int numBrickCol, int numBrickRow, int roundPassed) {
-		this.numBrickCol = numBrickCol;
-		startX = (WorldHeight / 2) / 6;
-		stopX = this.getStopX(roundPassed);
-		brickL = WorldWidth / numBrickRow;
-		brickH = (stopX - startX) / numBrickCol;
-		stopY = WorldWidth - 7.5;
-	}
+  private int numBrickCol;
+  private Double startX;
+  private Double startY = 0d;
+  private Double stopX;
+  private Double stopY;
+  private Double brickL;
+  private Double brickH;
 
-	/**
-	 * 
-	 * @param numR number of rounds passed
-	 * @return returns the height at which to stop the placement of the bricks
-	 */
-	private Double getStopX(int numR) {
-		if (numBrickCol > 6) {
-			return (((WorldHeight / 2) / 3) * 1.75);
-		} else if (numBrickCol > 4) {
-			return (((WorldHeight / 2) / 3) * 1.5);
-		} else {
-			return ((WorldHeight / 2) / 3);
-		}
-	}
+  /**
+   * costructor of this class.
+   * 
+   * @param numBrickCol number of bricks in a column
+   * @param numBrickRow number of brick in a row
+   * @param roundPassed number of rounds passed
+   */
+  public SizeCalculation(final int numBrickCol, final int numBrickRow,
+      final int roundPassed) {
+    this.numBrickCol = numBrickCol;
+    startX = (WORLD_HEIGHT / 2) / DIVIDER_X;
+    stopX = this.getStopX(roundPassed);
+    brickL = WORLD_WIDTH / numBrickRow;
+    brickH = (stopX - startX) / numBrickCol;
+    stopY = WORLD_WIDTH - SPACE_Y;
+  }
 
-	/**
-	 * 
-	 * @return the size of the model
-	 */
-	public static Pair<Double, Double> getWorldSize() {
-		return new Pair<Double, Double>(WorldHeight, WorldWidth);
-	}
+  /**
+   * 
+   * @param numR number of rounds passed.
+   * @return returns the height at which to stop the placement of the bricks.
+   */
+  private Double getStopX(final int numR) {
+    if (numBrickCol > NUMCOL) {
+      return (((WORLD_HEIGHT / 2) / 3) * MUL_X1);
+    } else if (numBrickCol > 4) {
+      return (((WORLD_HEIGHT / 2) / 3) * MUL_X2);
+    } else {
+      return ((WORLD_HEIGHT / 2) / 3);
+    }
+  }
 
-	/**
-	 * 
-	 * @return the starting coordinates from which to start the positioning of the
-	 *         bricks
-	 */
-	public Pair<Double, Double> getStart() {
-		return new Pair<Double, Double>(startX, startY);
-	}
+  /**
+   * 
+   * @return the size of the model.
+   */
+  public static Pair<Double, Double> getWorldSize() {
+    return new Pair<Double, Double>(WORLD_HEIGHT, WORLD_WIDTH);
+  }
 
-	/**
-	 * 
-	 * @return the end coordinates from which to stop the positioning of the bricks
-	 */
-	public Pair<Double, Double> getStop() {
-		return new Pair<Double, Double>(stopX, stopY);
-	}
+  /**
+   * 
+   * @return the starting coordinates from which to start the positioning of the
+   *         bricks.
+   */
+  public Pair<Double, Double> getStart() {
+    return new Pair<Double, Double>(startX, startY);
+  }
 
-	/**
-	 * 
-	 * @return the brick dimension
-	 */
-	public Pair<Double, Double> getBrickDim() {
-		return new Pair<Double, Double>(brickH, brickL);
-	}
+  /**
+   * 
+   * @return the end coordinates from which to stop the positioning of the bricks.
+   */
+  public Pair<Double, Double> getStop() {
+    return new Pair<Double, Double>(stopX, stopY);
+  }
 
-	/**
-	 * 
-	 * @param x number of row
-	 * @return method used to know the y of the rows of bricks
-	 */
-	public Double getRowCordinate(Double x) {
-		return this.startX + (x * this.brickH);
-	}
+  /**
+   * 
+   * @return the brick dimension.
+   */
+  public Pair<Double, Double> getBrickDim() {
+    return new Pair<Double, Double>(brickH, brickL);
+  }
 
-	/**
-	 * 
-	 * @return the dimension of pad method that calculates the size of the pad
-	 */
-	public Dimension getPadDim() {
-		return new DimensionImpl(WorldHeight / 60, WorldWidth / 4);
-	}
+  /**
+   * 
+   * @param x number of row.
+   * @return method used to know the y of the rows of bricks
+   */
+  public Double getRowCordinate(final Double x) {
+    return this.startX + (x * this.brickH);
+  }
 
-	/**
-	 * 
-	 * @return the dimension of ball method that calculates the size of the ball
-	 */
-	public Dimension getBallDim() {
-		return new DimensionImpl(WorldWidth / 30, WorldWidth / 30);
-	}
+  /**
+   * 
+   * @return the dimension of pad method that calculates the size of the pad.
+   */
+  public Dimension getPadDim() {
+    return new DimensionImpl(WORLD_HEIGHT / DIVEDER_PAD_X, WORLD_WIDTH / 4);
+  }
+
+  /**
+   * 
+   * @return the dimension of ball method that calculates the size of the ball.
+   */
+  public Dimension getBallDim() {
+    return new DimensionImpl(WORLD_WIDTH / DIVEDER_BALL, WORLD_WIDTH / DIVEDER_BALL);
+  }
 }
