a6ee06b1145bbc5b9e0f273db2c90ebd3cae48b4
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2023-04-01 19:16:14 +0200

    Implemented HardBrick test

--- src/test/java/it/unibo/game/app/model/BrickTest.java
+++ src/test/java/it/unibo/game/app/model/BrickTest.java
@@ -1,14 +1,20 @@
 package it.unibo.game.app.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
 import it.unibo.game.app.api.Brick;
 import it.unibo.game.app.api.BrickType;
 import it.unibo.game.app.api.Level;
+import it.unibo.game.app.model.levels.SecondLevel;
 import it.unibo.game.app.model.levels.ThirdLevel;
 
 public class BrickTest {
@@ -36,5 +42,17 @@ public class BrickTest {
     assertTrue(l.getRound().getBrick().size() == (int) obstacles && l.getRound()
         .getBrick().stream().allMatch(x -> x.getType().equals(BrickType.OBSTACLE)));
 
+  }
+
+  @Test
+  void testHardBrick() {
+    Level l = new SecondLevel();
+    List<Integer> indexHardBrick = new ArrayList<>();
+    indexHardBrick = l.getRound().getBrick().stream().filter(b -> b.getRes().get() == 2)
+        .map(b -> l.getRound().getBrick().indexOf(b)).collect(Collectors.toList());
+    for (var i : indexHardBrick) {
+      l.getRound().getBrick().get(i).hit();
+      assertEquals(1, l.getRound().getBrick().get(i).getRes().get());
+    }
   }
 }
