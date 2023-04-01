8eb8c33614a4718c261e51badbc3e41ff587646b
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-04-01 09:26:37 +0200

    Checked code style in RoundEasy

--- src/main/java/it/unibo/game/app/model/round/RoundEasy.java
+++ src/main/java/it/unibo/game/app/model/round/RoundEasy.java
@@ -6,37 +6,57 @@ import it.unibo.game.app.model.DimensionImpl;
 import it.unibo.game.app.model.SizeCalculation;
 import it.unibo.game.app.model.brick.NormalBrick;
 
+/**
+ * Defines how a round easy is designed.
+ */
 public class RoundEasy extends AbstractRound {
 
-    private Double startX;
-    private Double startY; 
-    private Double endX;
-    private Double endY;
-    private int numSurpriseBrick = 0;
+  private Double startX;
+  private Double startY;
+  private Double endX;
+  private Double endY;
+  private int numSurpriseBrick = 0;
+  private static final int FIX_START_Y = 5;
+  private static final int FIX_END_Y = 3;
 
-    public RoundEasy(int numB, int numS, SizeCalculation size) {
-        super(numB, numS, size);
-        this.startY = size.getStart().getY() + (this.getSizeCalc().getBrickDim().getY() / 2) - 5;
-        this.startX = size.getStart().getX();
-        this.endY = SizeCalculation.getWorldSize().getY() - (3 *(this.getSizeCalc().getBrickDim().getY() / 2));
-        this.endX = size.getStop().getX();
-        this.setPosBrick();
-    }
+  /**
+   * Constructor of this class.
+   * 
+   * @param numB num of normal bricks
+   * @param numS num of surprise bricks
+   * @param size information of where to collocate bricks
+   */
+  public RoundEasy(final int numB, final int numS, final SizeCalculation size) {
+    super(numB, numS, size);
+    this.startY = size.getStart().getY() + (this.getSizeCalc().getBrickDim().getY() / 2)
+        - FIX_START_Y;
+    this.startX = size.getStart().getX();
+    this.endY = SizeCalculation.getWorldSize().getY()
+        - (FIX_END_Y * (this.getSizeCalc().getBrickDim().getY() / 2));
+    this.endX = size.getStop().getX();
+    this.setPosBrick();
+  }
 
-    @Override
-    public void setPosBrick() {
-        for(Double i = startX; i < endX; i = i + this.getSizeCalc().getBrickDim().getX()) {
-            for(Double j = startY; j < endY; j = j + this.getSizeCalc().getBrickDim().getY()) {
-							NormalBrick brick = new NormalBrick(BrickType.NORMAL, new DimensionImpl(getSizeCalc().getBrickDim().getX(),getSizeCalc().getBrickDim().getY()),new Pair<>(j,i),1);
-							brick.setPos(new Pair<>(j,i));
-                super.addBrick(brick);
-            }
-        }
-        while (this.numSurpriseBrick < this.getNumSur()) {
-            if (this.setBrickSurprise()) {
-                this.numSurpriseBrick++;
-            }
-        }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setPosBrick() {
+    for (Double i = startX; i < endX; i = i + this.getSizeCalc().getBrickDim().getX()) {
+      for (Double j = startY; j < endY; j = j + this.getSizeCalc().getBrickDim().getY()) {
+        NormalBrick brick = new NormalBrick(BrickType.NORMAL,
+            new DimensionImpl(getSizeCalc().getBrickDim().getX(),
+                getSizeCalc().getBrickDim().getY()),
+            new Pair<>(j, i), 1);
+        brick.setPos(new Pair<>(j, i));
+        super.addBrick(brick);
+      }
     }
-    
+    while (this.numSurpriseBrick < this.getNumSur()) {
+      if (this.setBrickSurprise()) {
+        this.numSurpriseBrick++;
+      }
+    }
+  }
+
 }
