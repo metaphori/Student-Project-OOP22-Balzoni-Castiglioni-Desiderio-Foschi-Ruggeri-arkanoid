64b94e2f2e6534d493a8fc79f669d82dfcc0b500
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-04-01 18:39:00 +0200

    estract method to peek color

--- src/main/java/it/unibo/game/app/view/jswing/implementation/GameViewImpl.java
+++ src/main/java/it/unibo/game/app/view/jswing/implementation/GameViewImpl.java
@@ -8,119 +8,138 @@ import it.unibo.game.app.view.jswing.api.UIController;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
+import java.util.Map.Entry;
+import java.util.Optional;
 
-public class GameViewImpl extends JPanel implements KeyListener, ActionListener, GameView {
+public class GameViewImpl extends JPanel
+    implements KeyListener, ActionListener, GameView {
 
-	private UIController observer;
-	// private boolean play = true;
+  private UIController observer;
+  // private boolean play = true;
 
-	public GameViewImpl(UIController control) {
-		setFocusable(true);
-		addKeyListener(this);
-		setFocusTraversalKeysEnabled(false);
-		this.observer = control;
-	}
+  public GameViewImpl(UIController control) {
+    setFocusable(true);
+    addKeyListener(this);
+    setFocusTraversalKeysEnabled(false);
+    this.observer = control;
+  }
 
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-		// Determina la dimensione del pannello
-		int panelWidth = getWidth();
-		int panelHeight = getHeight();
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D g2d = (Graphics2D) g;
+    // Determina la dimensione del pannello
+    int panelWidth = getWidth();
+    int panelHeight = getHeight();
 
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, panelWidth, panelHeight);
+    g.setColor(Color.WHITE);
+    g.fillRect(0, 0, panelWidth, panelHeight);
 
-		// delta di trasformazione
-		double deltaH = ((double) panelHeight / observer.getDimension().getX());
-		double deltaW = ((double) panelWidth / observer.getDimension().getY());
+    // delta di trasformazione
+    double deltaH = ((double) panelHeight / observer.getDimension().getX());
+    double deltaW = ((double) panelWidth / observer.getDimension().getY());
 
-		observer.getList().entrySet().stream().forEach(x -> {
-			g2d.setColor(x.getValue().isEmpty() ? Color.BLACK
-					: x.getValue().get() == 2 ? Color.LIGHT_GRAY
-							: x.getKey().getY().intValue() == observer.getRowC(0d).intValue() ? Color.RED
-									: x.getKey().getY().intValue() == observer.getRowC(1d).intValue() ? Color.BLUE
-											: x.getKey().getY().intValue() == observer.getRowC(2d).intValue() ? Color.YELLOW
-													: x.getKey().getY().intValue() == observer.getRowC(3d).intValue() ? Color.MAGENTA
-															: x.getKey().getY().intValue() == observer.getRowC(4d).intValue() ? Color.ORANGE
-																	: x.getKey().getY().intValue() == observer.getRowC(5d).intValue() ? Color.CYAN
-																			: Color.GREEN);
-			// g2d.draw(new Rectangle2D.Double(x.getKey().getX(),x.getKey().getY(),
-			// observer.getDimensionBrick().getY(), observer.getDimensionBrick().getX()));
-			var rec = new Rectangle2D.Double(x.getKey().getX(), x.getKey().getY(), observer.getDimensionBrick().getY(),
-					observer.getDimensionBrick().getX());
-			g2d.fill(rec);
-			g2d.setColor(Color.BLACK);
-			g2d.draw(rec);
-		});
+    observer.getList().entrySet().stream().forEach(x -> {
+      g2d.setColor(peekColor(x));
+      // g2d.draw(new Rectangle2D.Double(x.getKey().getX(),x.getKey().getY(),
+      // observer.getDimensionBrick().getY(), observer.getDimensionBrick().getX()));
+      var rec = new Rectangle2D.Double(x.getKey().getX(), x.getKey().getY(),
+          observer.getDimensionBrick().getY(), observer.getDimensionBrick().getX());
+      g2d.fill(rec);
+      g2d.setColor(Color.BLACK);
+      g2d.draw(rec);
+    });
 
     observer.getBall().stream().forEach(x -> {
-			g2d.setColor(Color.GREEN);
-			g2d.fill(new Ellipse2D.Double(x.getX(), x.getY(), observer.getRBall(), observer.getRBall()));
-		});
+      g2d.setColor(Color.GREEN);
+      g2d.fill(new Ellipse2D.Double(x.getX(), x.getY(), observer.getRBall(),
+          observer.getRBall()));
+    });
 
-		observer.getSurprise().stream().forEach(x -> {
-			g2d.setColor(Color.RED);
-			g2d.fill(new Ellipse2D.Double(x.getX(), x.getY(), observer.getRBall(), observer.getRBall()));
-		});
+    observer.getSurprise().stream().forEach(x -> {
+      g2d.setColor(Color.RED);
+      g2d.fill(new Ellipse2D.Double(x.getX(), x.getY(), observer.getRBall(),
+          observer.getRBall()));
+    });
 
-		g2d.setColor(Color.BLACK);
-		g2d.fill(new Rectangle2D.Double(observer.getPadPos().getX(), observer.getPadPos().getY(), observer.getPadWight(),
-				observer.getPadHeight()));
+    g2d.setColor(Color.BLACK);
+    g2d.fill(new Rectangle2D.Double(observer.getPadPos().getX(),
+        observer.getPadPos().getY(), observer.getPadWight(), observer.getPadHeight()));
 
-		g2d.setFont(new Font("myFont", Font.ITALIC, (int) (20 * deltaH)));
-		g2d.setColor(Color.RED);
-		g2d.drawString("SCORE: " + this.observer.getScore(), (int) (10 * deltaW), (int) (20 * deltaH));
+    g2d.setFont(new Font("myFont", Font.ITALIC, (int) (20 * deltaH)));
+    g2d.setColor(Color.RED);
+    g2d.drawString("SCORE: " + this.observer.getScore(), (int) (10 * deltaW),
+        (int) (20 * deltaH));
 
-		g2d.setFont(new Font("myFont", Font.ITALIC, (int) (20 * deltaH)));
-		g2d.setColor(Color.RED);
-		g2d.drawString("LIVES: " + this.observer.getLife(), (int) (225 * deltaW), (int) (20 * deltaH));
+    g2d.setFont(new Font("myFont", Font.ITALIC, (int) (20 * deltaH)));
+    g2d.setColor(Color.RED);
+    g2d.drawString("LIVES: " + this.observer.getLife(), (int) (225 * deltaW),
+        (int) (20 * deltaH));
 
-		g2d.dispose();
+    g2d.dispose();
 
-	}
+  }
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		repaint();
-	}
+  private Color peekColor(Entry<Pair<Double, Double>, Optional<Integer>> x) {
+    return x.getValue().isEmpty() ? Color.BLACK
+        : x.getValue().get() == 2 ? Color.LIGHT_GRAY
+            : x.getKey().getY().intValue() == observer.getRowC(0d).intValue() ? Color.RED
+                : x.getKey().getY().intValue() == observer.getRowC(1d).intValue()
+                    ? Color.BLUE
+                    : x.getKey().getY().intValue() == observer.getRowC(2d).intValue()
+                        ? Color.YELLOW
+                        : x.getKey().getY().intValue() == observer.getRowC(3d)
+                            .intValue()
+                                ? Color.MAGENTA
+                                : x.getKey().getY().intValue() == observer.getRowC(4d)
+                                    .intValue()
+                                        ? Color.ORANGE
+                                        : x.getKey().getY().intValue() == observer
+                                            .getRowC(5d).intValue() ? Color.CYAN
+                                                : Color.GREEN;
+  }
 
-	@Override
-	public void keyPressed(KeyEvent arg0) {
-		if (arg0.getKeyCode() == KeyEvent.VK_RIGHT) {
-			observer.movePadRight();
-		}
-		if (arg0.getKeyCode() == KeyEvent.VK_LEFT) {
-			observer.movePadLeft();
-		} else if (arg0.getKeyCode() == KeyEvent.VK_SPACE) {
-			observer.pauseMenu();
-		}
-	}
+  @Override
+  public void actionPerformed(ActionEvent arg0) {
+    repaint();
+  }
 
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		// throw new UnsupportedOperationException("Unimplemented method
-		// 'keyReleased'");
-	}
+  @Override
+  public void keyPressed(KeyEvent arg0) {
+    if (arg0.getKeyCode() == KeyEvent.VK_RIGHT) {
+      observer.movePadRight();
+    }
+    if (arg0.getKeyCode() == KeyEvent.VK_LEFT) {
+      observer.movePadLeft();
+    } else if (arg0.getKeyCode() == KeyEvent.VK_SPACE) {
+      observer.pauseMenu();
+    }
+  }
 
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
-	}
+  @Override
+  public void keyReleased(KeyEvent arg0) {
+    // TODO Auto-generated method stub
+    // throw new UnsupportedOperationException("Unimplemented method
+    // 'keyReleased'");
+  }
 
-	@Override
-	public void drawBall(Pair<Integer, Integer> pos, Integer radius, Graphics g) {
-		g.setColor(Color.WHITE);
-		g.drawOval(pos.getX() - radius, pos.getY() - radius, radius * 2, radius * 2);
-	}
+  @Override
+  public void keyTyped(KeyEvent arg0) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+  }
 
-	@Override
-	public void drawPad(Pair<Integer, Integer> pos, Pair<Integer, Integer> dimPad, Graphics g) {
-		g.setColor(Color.GRAY);
-		g.fillRect(pos.getX(), pos.getY(), dimPad.getX(), dimPad.getY());
-		g.drawRect(pos.getX(), pos.getY(), dimPad.getX(), dimPad.getY());
-	}
+  @Override
+  public void drawBall(Pair<Integer, Integer> pos, Integer radius, Graphics g) {
+    g.setColor(Color.WHITE);
+    g.drawOval(pos.getX() - radius, pos.getY() - radius, radius * 2, radius * 2);
+  }
+
+  @Override
+  public void drawPad(Pair<Integer, Integer> pos, Pair<Integer, Integer> dimPad,
+      Graphics g) {
+    g.setColor(Color.GRAY);
+    g.fillRect(pos.getX(), pos.getY(), dimPad.getX(), dimPad.getY());
+    g.drawRect(pos.getX(), pos.getY(), dimPad.getX(), dimPad.getY());
+  }
 }
