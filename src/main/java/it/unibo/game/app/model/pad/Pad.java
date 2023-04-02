225d54ddd5ae97eb38c44fd8531a222b3413e663
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-04-02 17:39:07 +0200

    delating pad physic

--- src/main/java/it/unibo/game/app/model/pad/Pad.java
+++ src/main/java/it/unibo/game/app/model/pad/Pad.java
@@ -12,17 +12,16 @@ import it.unibo.game.app.api.MovingObject;
 
 public class Pad extends AbstractMovingObject {
 
-	/*
-	 * le dimensioni sono modificabili per rendere resizable la dinestra
-	 * 
-	 */
+  /*
+   * le dimensioni sono modificabili per rendere resizable la dinestra
+   * 
+   */
 
-	public Pad(Dimension d) {
-		super(new Pair<>(SizeCalculation.getWorldSize().getY() / 2 - d.getWidth() / 2,
-				SizeCalculation.getWorldSize().getX() - 100), d);
-		super.setBoundingBox(new RectBoundingBox(this));
-		super.setSpeed(new SpeedImpl(10, 0));
-		super.setPhysics(new BallPhysicsImpl()); // ci vuole la fisica giusta
-	}
+  public Pad(Dimension d) {
+    super(new Pair<>(SizeCalculation.getWorldSize().getY() / 2 - d.getWidth() / 2,
+        SizeCalculation.getWorldSize().getX() - 100), d);
+    super.setBoundingBox(new RectBoundingBox(this));
+    super.setSpeed(new SpeedImpl(10, 0));
+  }
 
 }
