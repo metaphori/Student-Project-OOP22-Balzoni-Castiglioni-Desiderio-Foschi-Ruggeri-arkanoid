0495d9220fe5ded67d14e4635cb14928a6310bd1
Margherita <margherita.balzoni@studio.unibo.it>
2023-04-02 13:15:01 +0200

    fixed bug in collisionWithPad

--- src/main/java/it/unibo/game/app/model/dynamic/Collision.java
+++ src/main/java/it/unibo/game/app/model/dynamic/Collision.java
@@ -12,51 +12,53 @@ import it.unibo.game.app.model.CircleBoundingBox;
 import it.unibo.game.app.model.RectBoundingBox;
 
 public class Collision {
-    private Level level;
+  private Level level;
 
-    public Collision(Level lev){
-        this.level = lev;
-    }
-    public void CollideWithEdges(MovingObject b, Double h, Double w){
-        b.setBoundingBox(new CircleBoundingBox(b));
-        if(b.getBoundingBox().getBox().get(Corner.LEFT_DOWN).getX() <= 0.5 ||b.getBoundingBox().getBox().get(Corner.RIGHT_DOWN).getX() >= w-7.5){
-            b.getPhysics().changeDirection(Side.LEFT_RIGHT);
-        }
-         if(b.getBoundingBox().getBox().get(Corner.LEFT_UP).getY() <= 0.5 ){
-            b.getPhysics().changeDirection(Side.UP_DOWN);
-        }
-    }
+  public Collision(Level lev) {
+    this.level = lev;
+  }
 
-    public Optional<Integer> collideWithBrick(MovingObject b){
-			b.setBoundingBox(new CircleBoundingBox(b));
-        for (Brick obj : level.getRound().getBrick()) {
-           var opt = b.getBoundingBox().collideWith(obj.getBoundingBox());
-            
-            if(opt.isPresent()){
-                if(obj.getType()==BrickType.NORMAL){
-                       this.level.getScore().increaseScore();
-                }else{
-                    this.level.getScore().resetPoints();
-                }
-                b.getPhysics().changeDirection(opt.get());
-                return Optional.of(level.getRound().getBrick().indexOf(obj));
-            }
-        }
-        return Optional.empty();
+  public void CollideWithEdges(MovingObject b, Double h, Double w) {
+    b.setBoundingBox(new CircleBoundingBox(b));
+    if (b.getBoundingBox().getBox().get(Corner.LEFT_DOWN).getX() <= 0.5
+        || b.getBoundingBox().getBox().get(Corner.RIGHT_DOWN).getX() >= w - 7.5) {
+      b.getPhysics().changeDirection(Side.LEFT_RIGHT);
     }
+    if (b.getBoundingBox().getBox().get(Corner.LEFT_UP).getY() <= 0.5) {
+      b.getPhysics().changeDirection(Side.UP_DOWN);
+    }
+  }
 
-    public boolean CollideWithPad (MovingObject b, MovingObject p){
-			b.setBoundingBox(new CircleBoundingBox(b));
-        p.setBoundingBox(new RectBoundingBox(p));
-        if(b.getBoundingBox().collideWith(p.getBoundingBox()).equals(Optional.of(Side.UP_DOWN))) {
+  public Optional<Integer> collideWithBrick(MovingObject b) {
+    b.setBoundingBox(new CircleBoundingBox(b));
+    for (Brick obj : level.getRound().getBrick()) {
+      var opt = b.getBoundingBox().collideWith(obj.getBoundingBox());
 
-            b.getPhysics().changeDirection(Side.UP_DOWN);
-            return true;
+      if (opt.isPresent()) {
+        if (obj.getType() == BrickType.NORMAL) {
+          this.level.getScore().increaseScore();
+        } else {
+          this.level.getScore().resetPoints();
         }
-        return false;
+        b.getPhysics().changeDirection(opt.get());
+        return Optional.of(level.getRound().getBrick().indexOf(obj));
+      }
     }
-   
-    public int getScore(){
-        return this.level.getScore().getScore();
+    return Optional.empty();
+  }
+
+  public boolean CollideWithPad(MovingObject b, MovingObject p) {
+    b.setBoundingBox(new CircleBoundingBox(b));
+    p.setBoundingBox(new RectBoundingBox(p));
+    var opt = b.getBoundingBox().collideWith(p.getBoundingBox());
+    if (opt.isPresent()) {
+      b.getPhysics().changeDirection(opt.get());
+      return true;
     }
+    return false;
+  }
+
+  public int getScore() {
+    return this.level.getScore().getScore();
+  }
 }
