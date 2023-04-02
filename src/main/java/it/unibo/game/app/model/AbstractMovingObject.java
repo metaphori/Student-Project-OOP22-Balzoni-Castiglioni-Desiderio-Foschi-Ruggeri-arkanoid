50f7dbe484a811db07c4b40edba9e6e85256e3d3
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-04-02 13:14:11 +0200

    remove useless comments

--- src/main/java/it/unibo/game/app/model/AbstractMovingObject.java
+++ src/main/java/it/unibo/game/app/model/AbstractMovingObject.java
@@ -1,6 +1,5 @@
 package it.unibo.game.app.model;
 
-/*dodo*/
 import it.unibo.game.Pair;
 import it.unibo.game.app.api.BoundingBox;
 import it.unibo.game.app.api.Dimension;
@@ -11,65 +10,64 @@ import it.unibo.game.app.model.dynamic.SpeedImpl;
 
 public abstract class AbstractMovingObject implements MovingObject {
 
-	private Pair<Double, Double> pos;
-	private BoundingBox Bbox;
-	private Speed speed;
-	private Dimension d;
-	private Physics pysic;
+  private Pair<Double, Double> pos;
+  private BoundingBox Bbox;
+  private Speed speed;
+  private Dimension d;
+  private Physics pysic;
 
-	public AbstractMovingObject(Pair<Double, Double> pos, Dimension d) {
-		this.pos = pos;
-		this.d = d;
-		this.speed = new SpeedImpl(3, 3);
-	}
+  public AbstractMovingObject(Pair<Double, Double> pos, Dimension d) {
+    this.pos = pos;
+    this.d = d;
+    this.speed = new SpeedImpl(3, 3);
+  }
 
-	public void setDimension(Dimension d) {
-		this.d = d;
-	}
+  public void setDimension(Dimension d) {
+    this.d = d;
+  }
 
-	@Override
-	public Pair<Double, Double> getPos() {
-		return this.pos;
-	}
+  @Override
+  public Pair<Double, Double> getPos() {
+    return this.pos;
+  }
 
-	@Override
-	public BoundingBox getBoundingBox() {
-		return this.Bbox;
-	}
+  @Override
+  public BoundingBox getBoundingBox() {
+    return this.Bbox;
+  }
 
-	@Override
-	public void setBoundingBox(BoundingBox box) {
-		this.Bbox = box;
-	}
+  @Override
+  public void setBoundingBox(BoundingBox box) {
+    this.Bbox = box;
+  }
 
-	@Override
-	public void setPos(Pair<Double, Double> pos) {
-		this.pos = pos;
-	}
+  @Override
+  public void setPos(Pair<Double, Double> pos) {
+    this.pos = pos;
+  }
 
-	@Override
-	public Speed getSpeed() {
-		// TODO Auto-generated method stub
-		return this.speed;
-	}
+  @Override
+  public Speed getSpeed() {
+    return this.speed;
+  }
 
-	@Override
-	public void setSpeed(Speed vel) {
-		// TODO Auto-generated method stub
-		this.speed = vel;
-	}
+  @Override
+  public void setSpeed(Speed vel) {
 
-	@Override
-	public Dimension getDimension() {
-		// TODO Auto-generated method stub
-		return this.d;
-	}
+    this.speed = vel;
+  }
 
-	public Physics getPhysics() {
-		return this.pysic;
-	}
+  @Override
+  public Dimension getDimension() {
 
-	public void setPhysics(Physics phsycs) {
-		this.pysic = phsycs;
-	}
+    return this.d;
+  }
+
+  public Physics getPhysics() {
+    return this.pysic;
+  }
+
+  public void setPhysics(Physics phsycs) {
+    this.pysic = phsycs;
+  }
 }
