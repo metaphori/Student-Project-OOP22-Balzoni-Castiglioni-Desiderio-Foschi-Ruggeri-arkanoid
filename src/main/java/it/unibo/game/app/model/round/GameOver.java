b3afcb3872753874382f41bd85edbc57678932e2
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-04-02 10:29:15 +0200

    Checked code style in GameOver

--- src/main/java/it/unibo/game/app/model/round/GameOver.java
+++ src/main/java/it/unibo/game/app/model/round/GameOver.java
@@ -4,30 +4,50 @@ import it.unibo.game.app.api.Brick;
 import it.unibo.game.app.api.BrickType;
 import it.unibo.game.app.api.Round;
 
+/**
+ * This class checks round states.
+ */
 public class GameOver {
-    
-    protected Round round;
 
-    public GameOver(Round round) {
-        this.round = round;
-    } 
-    public boolean hasMissedBall() {
-        for (var ball : this.round.getBalls()) {
-          if(!(ball.getPos().getY() > this.round.getPad().getPos().getY())){
-            return false;
-          }
-        }
-        return true;
+  private Round round;
+
+  /**
+   * constructor of this class.
+   * 
+   * @param round represents the playing round
+   */
+  public GameOver(final Round round) {
+    this.round = round;
+  }
+
+  /**
+   * @return boolean value that indicates if the pad has missed the ball.
+   */
+  public boolean hasMissedBall() {
+    for (var ball : this.round.getBalls()) {
+      if (!(ball.getPos().getY() > this.round.getPad().getPos().getY())) {
+        return false;
+      }
     }
-    public boolean isRoundFinished() {
-        return (this.round.getBrick().size() < 1 || remainsOnlyObstacles()) ? true : false;
+    return true;
+  }
+
+  /**
+   * @return boolean value that indicates if the round is finished.
+   */
+  public boolean isRoundFinished() {
+    return this.round.getBrick().size() < 1 || remainsOnlyObstacles();
+  }
+
+  /**
+   * @return boolean value that indicates if remains only obstacles in round.
+   */
+  public boolean remainsOnlyObstacles() {
+    for (Brick element : this.round.getBrick()) {
+      if (element.getType() != BrickType.OBSTACLE) {
+        return false;
+      }
     }
-    public boolean remainsOnlyObstacles() {
-        for (Brick element : this.round.getBrick()) {
-            if(element.getType() != BrickType.OBSTACLE) {
-                return false;
-            }
-        }
-        return true;
-    }
+    return true;
+  }
 }
