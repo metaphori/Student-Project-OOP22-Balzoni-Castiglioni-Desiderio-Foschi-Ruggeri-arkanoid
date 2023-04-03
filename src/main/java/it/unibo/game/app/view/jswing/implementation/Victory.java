8bf68e3fba1d63db9734383e9e882be7db8d5919
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2023-04-03 08:51:01 +0200

    Removed repetead code in subclasses and implemented it in AbstractView

--- src/main/java/it/unibo/game/app/view/jswing/implementation/Victory.java
+++ src/main/java/it/unibo/game/app/view/jswing/implementation/Victory.java
@@ -15,7 +15,6 @@ public class Victory extends AbstractView {
   public Victory(final UIControllerImpl uiCtrl) {
     super(uiCtrl);
     this.getTitle().setText("YOU WIN");
-    this.getButtonsPanel().setLayout(new GridLayout(3, 1, 0, 1));
     this.getButtonsPanel().add(this.getSaveBtn());
     this.getButtonsPanel().add(this.getMenuBtn());
     this.getButtonsPanel().add(this.getQuitBtn());
