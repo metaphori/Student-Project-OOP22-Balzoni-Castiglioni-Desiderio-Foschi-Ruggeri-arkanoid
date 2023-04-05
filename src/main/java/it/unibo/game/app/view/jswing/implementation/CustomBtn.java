ebb9e4d525317bc0dbd0e0c50bfe3276293982dc
Margherita Balzoni - margherita.balzoni@studio.unibo.it <margherita.balzoni@studio.unibo.it>
2023-04-05 15:01:55 +0200

    Fixed style in CustomBtn

--- src/main/java/it/unibo/game/app/view/jswing/implementation/CustomBtn.java
+++ src/main/java/it/unibo/game/app/view/jswing/implementation/CustomBtn.java
@@ -4,10 +4,19 @@ import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JButton;
 
+/**
+ * Class that creates custom buttons.
+ */
 public class CustomBtn extends JButton {
 
 	private Font btnFont;
 
+	/**
+	 * Constructor of this class.
+	 * 
+	 * @param size size of the button.
+	 * @param text text of the button.
+	 */
 	public CustomBtn(int size, String text) {
 		setText(text);
 		btnFont = new Font("arial", Font.BOLD, size);
