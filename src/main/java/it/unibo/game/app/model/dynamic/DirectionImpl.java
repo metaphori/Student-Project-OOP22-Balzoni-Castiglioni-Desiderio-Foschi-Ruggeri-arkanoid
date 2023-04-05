005766ac0b0dc28cb7eb905c6480192df8cba506
Margherita Balzoni - margherita.balzoni@studio.unibo.it <margherita.balzoni@studio.unibo.it>
2023-04-05 14:51:27 +0200

    Fixed style in DirectionImpl

--- src/main/java/it/unibo/game/app/model/dynamic/DirectionImpl.java
+++ src/main/java/it/unibo/game/app/model/dynamic/DirectionImpl.java
@@ -7,94 +7,94 @@ import it.unibo.game.app.api.Direction;
  * Class that manages the direction of MovingObject.
  */
 public class DirectionImpl implements Direction {
-  private static final int LEFT = -1;
-  private static final int RIGHT = 1;
-  private static final int UP = -1;
-  private static final int DOWN = 1;
-  private Pair<Integer, Integer> d;
+	private static final int LEFT = -1;
+	private static final int RIGHT = 1;
+	private static final int UP = -1;
+	private static final int DOWN = 1;
+	private Pair<Integer, Integer> d;
 
-  /**
-   * 
-   */
-  public DirectionImpl() {
-    this.resetDirection();
-  }
+	/**
+	 * constructor of this class
+	 */
+	public DirectionImpl() {
+		this.resetDirection();
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  public void setDirection(Pair<Integer, Integer> newD) {
-    this.d = newD;
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setDirection(final Pair<Integer, Integer> newD) {
+		this.d = newD;
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void setDirectionUp() {
-    this.d = new Pair<Integer, Integer>(this.d.getX(), UP);
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setDirectionUp() {
+		this.d = new Pair<Integer, Integer>(this.d.getX(), UP);
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void setDirectionDown() {
-    this.d = new Pair<Integer, Integer>(this.d.getX(), DOWN);
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setDirectionDown() {
+		this.d = new Pair<Integer, Integer>(this.d.getX(), DOWN);
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void setDirectionLeft() {
-    this.d = new Pair<Integer, Integer>(LEFT, this.d.getY());
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setDirectionLeft() {
+		this.d = new Pair<Integer, Integer>(LEFT, this.d.getY());
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void setDirectionRight() {
-    this.d = new Pair<Integer, Integer>(RIGHT, this.d.getY());
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setDirectionRight() {
+		this.d = new Pair<Integer, Integer>(RIGHT, this.d.getY());
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean isDirectionUp() {
-    return (this.d.getY() == UP) ? true : false;
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isDirectionUp() {
+		return this.d.getY() == UP;
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean isDirectionLeft() {
-    return (this.d.getX() == LEFT) ? true : false;
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isDirectionLeft() {
+		return this.d.getX() == LEFT;
+	}
 
-  /** {@inheritDoc} */
-  @Override
-  public boolean isDirectionRight() {
-    return this.d.getX().equals(RIGHT);
-  }
+	/** {@inheritDoc} */
+	@Override
+	public boolean isDirectionRight() {
+		return this.d.getX().equals(RIGHT);
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public Pair<Integer, Integer> getDirection() {
-    return this.d;
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Pair<Integer, Integer> getDirection() {
+		return this.d;
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void resetDirection() {
-    this.d = new Pair<Integer, Integer>(LEFT, UP);
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void resetDirection() {
+		this.d = new Pair<Integer, Integer>(LEFT, UP);
+	}
 
 }
