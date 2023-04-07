7f299401ceea624e1d7f0505a1604ee8c375631d
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-04-07 20:14:16 +0200

    delete interface UpdateView beacause it was unused

--- src/main/java/it/unibo/game/app/view/jswing/api/UpdateView.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.game.app.view.jswing.api;
-import java.awt.*;
-import it.unibo.game.Pair;
-
-public interface UpdateView {
-    void update(Graphics g, Pair<Integer, Integer> dimension, Pair<Integer, Integer> pos, GameView view);
-}
